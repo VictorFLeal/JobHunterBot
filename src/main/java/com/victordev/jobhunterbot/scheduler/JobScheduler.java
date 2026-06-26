17f09df7cd728c58cfe74d73afa18ea90efcc598
Victor Leal <victorlealdev@gmail.com>
2026-06-26 15:24:20 -0400

    feat: add JobScheduler with 30min scheduled task

--- src/main/java/com/victordev/jobhunterbot/scheduler/JobScheduler.java
+++ src/main/java/com/victordev/jobhunterbot/scheduler/JobScheduler.java
@@ -1,4 +1,15 @@
 package com.victordev.jobhunterbot.scheduler;
 
+import com.victordev.jobhunterbot.service.JobSearchService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JobScheduler {
+    private JobSearchService jobSearchService;
+
+    @Scheduled(fixedRate = 1800000)
+    public void run(){
+        jobSearchService.searchJobs();
+    }
 }
