c027083f826103009260e5d8a47eb67135c9d89b
Victor Leal <victorlealdev@gmail.com>
2026-06-26 15:39:43 -0400

    feat: add TelegramService structure and ignore application.yml

--- src/main/java/com/victordev/jobhunterbot/telegram/TelegramService.java
+++ src/main/java/com/victordev/jobhunterbot/telegram/TelegramService.java
@@ -1,4 +1,19 @@
 package com.victordev.jobhunterbot.telegram;
 
+import com.victordev.jobhunterbot.model.Job;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TelegramService {
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Value("${telegram.bot.chatId}")
+    private String chatId;
+
+    Job sendMessage(String message){
+        Job job = new Job();
+        return job;
+    }
 }
