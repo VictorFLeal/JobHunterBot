f16d15002d02f3eee29f4879da639bc28cbd722f
Victor Leal <victorlealdev@gmail.com>
2026-06-26 09:02:32 -0400

    feat: add LinkedInParser with parse method

--- src/main/java/com/victordev/jobhunterbot/parser/LinkedInParser.java
+++ src/main/java/com/victordev/jobhunterbot/parser/LinkedInParser.java
@@ -1,4 +1,9 @@
 package com.victordev.jobhunterbot.parser;
 
+import com.victordev.jobhunterbot.model.Job;
+
 public class LinkedInParser {
+    public Job parse (String html){
+        return null;
+    }
 }
